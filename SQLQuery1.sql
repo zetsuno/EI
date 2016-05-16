91f0472b5149c49e6349b6dae0f0b42d341f6e70
zetsuno <daniel_alex_fernandes@hotmail.com>
2016-05-16 11:42:28 +0100

    Why the fuck you lyin'?
    Why you always lyin'?

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Finalidade] NVARCHAR(MAX) NOT NULL , 
+    [Orçamento] MONEY NOT NULL, 
+    [Data_Limite] DATE NULL, 
+    [Nota_Pessoal] NVARCHAR(MAX) NULL, 
+    [Montagem] BINARY(50) NULL, 
+    [Request_ID] INT NOT NULL, 
+    PRIMARY KEY ([Request_ID])
+)
