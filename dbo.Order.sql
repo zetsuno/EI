8a8e127b6b6fbbb9ea0abddb20a839f9841b72fd
zetsuno <daniel_alex_fernandes@hotmail.com>
2016-05-18 23:06:41 +0100

    updates#2

---
+++ dbo.Order.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Order] (
+    [Finalidade]   NVARCHAR (MAX) NOT NULL,
+    [Orçamento]    MONEY          NOT NULL,
+    [Data_Limite]  DATE           NULL,
+    [Nota_Pessoal] NVARCHAR (MAX) NULL,
+    [Montagem]     BINARY (50)    NULL,
+    [Request_ID]   INT            NOT NULL,
+    [Min_HDD] NVARCHAR(MAX) NULL, 
+    [Min_RAM] NVARCHAR(MAX) NULL, 
+    [Monitor] NVARCHAR(MAX) NULL, 
+    PRIMARY KEY CLUSTERED ([Request_ID] ASC)
+);
+
